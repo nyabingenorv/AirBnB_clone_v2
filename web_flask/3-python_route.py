91ece363b84abe2a34a7b5166f53faef7b4126eb
nyabingenorv <norvyphill@gmail.com>
2024-03-24 14:40:14 +0300

    web_flask; rev 2;python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web app listening on 0.0.0.0:5000.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a welcome string"""
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displays "C " followed by the value of the text variable"""
+
+    updated_text = text.replace('_', ' ')
+    return 'C ' + updated_text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """Displays "Python " followed by the value of the text variable"""
+
+    updated_text = text.replace('_', ' ')
+    return 'Python ' + updated_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
